8e1ddf6db3bdca26d1f402027ff17327a62adbf7
Chris Rose <offbyone@github.com>
2023-03-30 09:41:20 -0700

    New module: formatter overrides
    The main thing here is to fix format-all's use of `with-temp-buffer`
    which runs into https://github.com/purcell/envrc/issues/12
    
    While I could submit a patch to format-all-the-things mode (per
    https://github.com/rust-lang/rust-mode/pull/447) I elected to fix it
    locally since Doom is dropping the library, and as a result I'm on an
    older version of the code anyway :/
    
    (see https://github.com/doomemacs/doomemacs/pull/6312 etc; format-all is
    in the [do-not-PR list](https://discourse.doomemacs.org/t/do-not-pr/))

---
+++ offby1/format/config.el
@@ -0,0 +1,9 @@
+;;; config.el --- Configure some core behaviour for format-all  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Chris Rose
+
+;; Author: Chris Rose <offbyone@github.com>
+;; Keywords: languages,
+
+(after! format-all
+  (inheritenv-add-advice 'format-all--buffer-thunk))
