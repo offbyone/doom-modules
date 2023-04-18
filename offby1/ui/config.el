d80903a066118d589b1ee208a6626b9a560b8a5a
Chris Rose <offbyone@github.com>
2023-04-17 19:08:02 -0700

    emacs: add some extra maps for embark/ace

--- offby1/ui/config.el
+++ offby1/ui/config.el
@@ -1,3 +1,25 @@
 ;; Overrides for my own help map
 (define-key! help-map
   "i"      #'consult-info)
+
+
+;;; Experimenting with some Embark-isms
+;;; Based heavily on [[https://karthinks.com/software/fifteen-ways-to-use-embark/][Fifteen ways to use Embark | Karthinks]]
+
+(eval-when-compile
+  (defmacro offby1/embark-ace-action (fn)
+    `(defun ,(intern (concat "offby1/embark-ace-" (symbol-name fn))) ()
+       (interactive)
+       (with-demoted-errors "%s"
+         (require 'ace-window)
+         (let ((aw-dispatch-always t))
+           (aw-switch-to-window (aw-select nil))
+           (call-interactively (symbol-function ',fn)))))))
+
+(use-package! embark
+  :defer t
+  :init
+  (map! (:map embark-file-map
+         :desc "Find file into window"  "o" (offby1/embark-ace-action find-file)))
+  (map! (:map embark-buffer-map
+         :desc "Switch to buffer in window"  "o" (offby1/embark-ace-action switch-to-buffer))))
