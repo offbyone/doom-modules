b6eb486fb3d05ba18f82044194743bb6c0ba084f
Chris Rose <offbyone@github.com>
2023-04-24 15:49:30 -0600

    fix up chatgpt-shell packages

--- offby1/exp/config.el
+++ offby1/exp/config.el
@@ -1,6 +1,7 @@
 (use-package! chatgpt-shell
   :if (modulep! +chatgpt)
   :config
+  (setq dall-e-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com")))
   (setq chatgpt-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com"))))
 
 (after! (ob chatgpt-shell)
