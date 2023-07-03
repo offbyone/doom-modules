947f4db0c871ad576b052b10db0e87a8560f4182
Chris Rose <offline@offby1.net>
2023-07-03 12:53:36 -0700

    added +consult-gh

--- offby1/git/config.el
+++ offby1/git/config.el
@@ -8,3 +8,17 @@
   :after magit
   :config
   (setq magit-delta-default-dark-theme "zenburn"))
+
+(use-package! consult-gh
+  :when (modulep! +consult)
+  :after consult
+  :config
+
+  ;;add your main GitHub account (replace "armindarvish" with your user or org)
+  (add-to-list 'consult-gh-default-orgs-list "offbyone")
+
+  ;;use "gh org list" to get a list of all your organizations and adds them to default list
+  (setq consult-gh-default-orgs-list (append consult-gh-default-orgs-list (remove "" (split-string (consult-gh--command-to-string "org" "list") "\n"))))
+
+  ;; set the default folder for cloning repositories, By default Consult-GH will confirm this before cloning
+  (setq consult-gh-default-clone-directory "~/projects"))
--- offby1/git/packages.el
+++ offby1/git/packages.el
@@ -2,3 +2,7 @@
 
 (when (modulep! +magit-delta)
   (package! magit-delta))
+
+(when (modulep! +consult)
+  (package! consult-gh
+    :recipe (:type git :host github :repo "armindarvish/consult-gh")))
