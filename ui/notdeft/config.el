45650851170a180c94a7151dd226e3011a47bbe5
Chris Rose <offbyone@github.com>
2024-02-12 09:59:33 -0800

    notdeft fixes:
    - handle the xapian path when we haven't previously tried to build
      xapian
    - replace the doom notdeft binding late

--- ui/notdeft/config.el
+++ ui/notdeft/config.el
@@ -1,8 +1,18 @@
 ;;; ui/deft/config.el -*- lexical-binding: t; -*-
 
 (use-package! notdeft
+  :commands (notdeft)
   :config
-  (setq notdeft-xapian-program (concat notdeft-xapian-home "notdeft-xapian"))
+  (setq notdeft-extension "org")
+  (setq notdeft-xapian-program (concat (if (boundp 'notdeft-xapian-home)
+                                           (concat notdeft-xapian-home "notdeft-xapian")
+                                         ;; we haven't loaded this, so we make our own
+                                         (expand-file-name "xapian/" (file-name-directory (file-truename (locate-library "notdeft")))))
+                                       "notdeft-xapian"))
   (setq notdeft-xapian-program-compile-command-format "g++ -o %s %s -std=c++11 -Wall `pkg-config --cflags --libs tclap` `xapian-config --cxxflags --libs`")
   (add-hook 'notdeft-load-hook 'notdeft-xapian-make-program-when-uncurrent))
 
+(add-hook! 'doom-after-init-hook
+  (map! :leader
+        (:prefix-map ("n" . "notes")
+         :desc "Notdeft" "d" #'notdeft)))
