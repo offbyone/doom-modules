7e284e70178282e894e88574f4665f61f5ca1648
Chris Rose <offline@offby1.net>
2024-09-06 10:27:12 -0500

    Conner support with fallback to compile-project

--- offby1/project/config.el
+++ offby1/project/config.el
@@ -1,13 +1,12 @@
 (use-package! conner
-  :when (modulep! +conner)
-  :bind (("C-x p C" . conner-run-project-command)))
+  :when (modulep! +conner))
 
-;; define a function that runs conner-run-project-command when called, but when called with the universal argument, runs projectile-run-shell-command-in-root
+;; define a function that runs conner-run-project-command when called, but when called with the universal argument, runs projectile-compile-project instead
 (defun offby1/run-project-command (&optional prefix)
   "Run a conner command in the project root. With a prefix argument, run a shell command in the project root."
   (interactive "P")
   (if prefix
-      (call-interactively #'projectile-run-shell-command-in-root)
+      (call-interactively #'projectile-compile-project)
     (call-interactively #'conner-run-project-command)))
 
 (after! (conner projectile)
