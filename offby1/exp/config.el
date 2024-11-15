324e753c0a863c954c9c2a6d422b9e62aa282390
Chris Rose <offline@offby1.net>
2024-11-15 00:14:48 -0800

    chatgpt-shell: modernize the import

--- offby1/exp/config.el
+++ offby1/exp/config.el
@@ -4,6 +4,7 @@
   (setq dall-e-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com")))
   (setq chatgpt-shell-openai-key (lambda () (auth-source-pick-first-password :host "api.openai.com"))))
 
-(after! (ob chatgpt-shell)
-  (require 'ob-chatgpt-shell)
+(use-package! ob-chatgpt-shell
+  :if (modulep! +ob-chatgpt)
+  :config
   (ob-chatgpt-shell-setup))
