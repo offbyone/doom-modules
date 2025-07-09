281626d5d9ac3b8083d0fe0f24d78b7959ceb375
Chris Rose <offline@offby1.net>
2025-07-09 13:11:21 -0700

    jj: use vc-jj and jujutsushi

--- offby1/jujutsu/config.el
+++ offby1/jujutsu/config.el
@@ -1,5 +1,11 @@
-(use-package! jujutsu)
+;; (use-package! jujutsu)
 
 (use-package! project
   :config
   (add-to-list 'project-vc-extra-root-markers ".jj"))
+
+(use-package! vc-jj)
+(use-package! jujutsushi)
+
+;; (after! git-commit
+;;   (setq git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\|jjdescription\\)\\'"))
