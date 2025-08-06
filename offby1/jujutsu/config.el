aed1dca346a5e6b7a929832824b34a5e6b3f0b8a
Chris Rose <offline@offby1.net>
2025-08-06 10:38:48 -0700

    jujutsu: fix commit editing

--- offby1/jujutsu/config.el
+++ offby1/jujutsu/config.el
@@ -7,5 +7,17 @@
 (use-package! vc-jj)
 (use-package! jujutsushi)
 
-;; (after! git-commit
-;;   (setq git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\|jjdescription\\)\\'"))
+(after! git-commit
+  (setq git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\|jjdescription\\)\\'"))
+
+;; Fix log-edit-done for jujutsu description files
+(defun +jujutsu-log-edit-done ()
+  "Finish editing jujutsu description and exit emacsclient."
+  (interactive)
+  (save-buffer)
+  (server-edit))
+
+(add-hook! 'log-edit-mode-hook
+  (when (and (buffer-file-name)
+             (string-match-p "jjdescription" (buffer-file-name)))
+    (setq-local log-edit-callback '+jujutsu-log-edit-done)))
