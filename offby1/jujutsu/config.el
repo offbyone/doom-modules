09396c2d74c0d95b5b928ac42706bc3bb712a1c4
Chris Rose <offline@offby1.net>
2025-08-30 07:24:22 -0700

    disable jujutsushi mode - not working as of most recent pull

--- offby1/jujutsu/config.el
+++ offby1/jujutsu/config.el
@@ -5,7 +5,8 @@
   (add-to-list 'project-vc-extra-root-markers ".jj"))
 
 (use-package! vc-jj)
-(use-package! jujutsushi)
+;; this fails on load due to `(void-variable jujutsushi-dispatch)` in jujutsushi.el
+;; (use-package! jujutsushi)
 
 (after! git-commit
   (setq git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\|jjdescription\\)\\'"))
